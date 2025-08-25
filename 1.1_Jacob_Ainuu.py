e2541e9ecfe5b0efcbd9d098b8fcc4c2ab12d46e
CYBERKING-hub <jacoba.24257@obhs.school.nz>
2025-08-25 12:32:41 +1200

    i made some commentings

--- 1.1_Jacob_Ainuu.py
+++ 1.1_Jacob_Ainuu.py
@@ -2,10 +2,44 @@
 
 Author: Jacob Ainuu
 Date: 25/08/2025
+Variation: 1
 Describtion: DGT Internal
 
 '''
 
 
+#-------------------------
+#comment first
+#input needed - age name and statistics from their fitness
+#output needed - greet the user. tell them about the progress and what it will do 
 
-#----------------------------
+
+
+
+
+#---------Libaries----------------
+
+user_name = ()
+user_age = ()
+valid = True
+
+
+
+
+
+#----------Funtions---------------
+#---------------------------------------------------
+#this funtion to find the users name and make users go and get started in user_name make sures the user
+#----------------------------------------------------
+
+def force_name():
+    while(valid):
+        user_name = input('Enter your name: ')
+        if len(user_name) > 2 and len()
+    
+
+
+
+
+
+#-----------main routine
