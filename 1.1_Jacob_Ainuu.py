64992761264947f2e8894aab218b85f3adbebdfe
CYBERKING-hub <jacoba.24257@obhs.school.nz>
2025-08-26 15:04:34 +1200

    Update 1.1_Jacob_Ainuu.py
    i have been trying to loop and put it in a list

--- 1.1_Jacob_Ainuu.py
+++ 1.1_Jacob_Ainuu.py
@@ -17,7 +17,24 @@ Describtion: fitness tracker
 
 User_name = str(input("Enter your name"))
 age = int(input("Enter your age"))
-Workout_result = int(input("Enter your press up result"))
+
+#this is a list, list always start counting from 0
+Workout_result_list = ["Push ups", "Pull ups", "Running", "Squats", "Jumping Jacks"]
+
+    print(Workout_result_list) #prints every students
+
+count = 0 #creating a count
+while count<len(Workout_result_list):
+    print(count, Workout_result_list[count])
+    count+=5
+print("This are all the results")
+
+print(Workout_result_list[0]) 
+print(Workout_result_list[1])
+print(Workout_result_list[2])
+print(Workout_result_list[3])
+print(Workout_result_list[4])
+
 
       
 
