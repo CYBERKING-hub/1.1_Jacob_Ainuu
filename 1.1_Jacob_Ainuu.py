aaecb2fe82f8dd041efdf405085f89da99658ca3
CYBERKING-hub <jacoba.24257@obhs.school.nz>
2025-09-05 13:57:42 +1200

    i did mu resub because i didnt do my commenting and now i just finished doing  my commenting

--- 1.1_Jacob_Ainuu.py
+++ 1.1_Jacob_Ainuu.py
@@ -15,24 +15,38 @@ Describtion: fitness tracker
 
 #---------Libaries----------------
 
+#ask for the users name
 User_name = str(input("Enter your name"))
+
+#ask for the users age
 age = int(input("Enter your age"))
 
+
+#create an empty list to store push-up results
 push_up_result=[]
 
+# Loop 5 times to get 5 push ups results from the users
 for i in range(5):
-        result = int(input(f"Enter push up result"))
-        push_up_result.append(result)
+        result = int(input(f"Enter push up result")) # ask for the users push up result
+        push_up_result.append(result)                # Display all push up result
         
-print("-Push-up result")
-print("Result", push_up_result)
+#----------------Displayimg Results---------------
 
+print("-Push-up result")      # Header for result
+print("Result", push_up_result) #  Display all push up result
+
+# calculate the average of the push up result
 average = sum(push_up_result) / len(push_up_result)
+
+# Dispplay average, rounded to 2 decimal places
 print("average push-ups:", round(average, 2))
 
+# Find and display the highes push up result
 best_result = max(push_up_result)
 print("Best push-ups results:", best_result)
 
+#-------------Feedback section----------------
+# provide feedback based on average performance
 print("Feedback")
 if average >= 40:
         print("Ecellent Work", User_name + "Your doing well")
